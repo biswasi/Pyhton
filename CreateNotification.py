bf1e939e5f26cdbaaa3fb46775ba4af75b9e0cac
Irabati Biswas <74686753+biswasi@users.noreply.github.com>
2025-12-04 21:04:10 -0500

    Create CreateNotification.py
    Message publishing through email

---
+++ CreateNotification.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+   client = boto3.client('sns')
+   Topic_ARN = "arn:aws:sns:us-east-1:166607322176:ReminderTopic"
+   response_one = client.publish (
+      TargetArn = Topic_ARN,
+      Message = json.dumps({'Reminder-Type': "Weekly Reminder", 'Reminder': "Reminder 1", 'Destination': "Email" }),
+   )
+   response_two = client.publish (
+      TargetArn = Topic_ARN,
+      Message = json.dumps({'Reminder-Type': "Weekly Reminder", 'Reminder': "Reminder 2", 'Destination': "Email"}),
+   )
+   response_three = client.publish (
+      TargetArn = Topic_ARN,
+      Message = json.dumps({'Reminder-Type': "Daily Reminder", 'Reminder': "Reminder 3", 'Destination': "SQS"}),
+   )
+   return {
+      'statusCode': 200,
+      'body': json.dumps({'response_one': response_one, 'response_two': response_two, 'response_three': response_three })
+   }
